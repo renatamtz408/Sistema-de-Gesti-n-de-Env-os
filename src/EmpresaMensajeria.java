c80fee75c9824db9078e212f2d108b5bb3b107b7
Renata Martinez <25430048@uttcampus.edu.mx>
2026-07-10 13:14:17 -0600

    clase paqueteenvio

--- src/EmpresaMensajeria.java
+++ src/EmpresaMensajeria.java
@@ -2,29 +2,43 @@ import java.util.ArrayList;
 
 public class EmpresaMensajeria {
 
-    private ArrayList<Paquete> paquetes;
+    private ArrayList<paqueteenvio> listapaquetes;
 
     public EmpresaMensajeria() {
-        paquetes = new ArrayList<>();
+        listapaquetes = new ArrayList<>();
     }
 
-    public void registrarPaquete(Paquete paquete) {
-        paquetes.add(paquete);
+    public void registrarPaquete(paqueteenvio p) {
+        listapaquetes.add(p);
     }
 
     public void imprimirReporte() {
         double total = 0;
         System.out.println("REPORTE");
-        for (Paquete paquete : paquetes) {
-            System.out.println("Destinatario: " + paquete.getnombreDestinatario());
-            System.out.println("Peso: " + paquete.getpesoKg());
+        for (paqueteenvio p: listapaquetes) {
 
-            double costo = paquete.obtenerCostoEnvio();
+            String eenvio = "";
+            if (p.getEstrategiaenvio() instanceof EnvioEstandar){
+                eenvio= "Envio Estandar";
+            }else if (p.getEstrategiaenvio() instanceof EnvioExpress){
+                eenvio = "Envio Express";
+            } else if (p.getEstrategiaenvio() instanceof EnvioInternacional) {
+                eenvio = "Envio Internacional";
+            }
+
+            System.out.println("Destinatario: " + p.getnombreDestinatario());
+            System.out.println("Peso: " + p.getpesoKg() + " kg");
+            System.out.println("Tipo de envio: " + eenvio);
+
+            double costo = p.obtenerCostoEnvio();
 
             System.out.println("Costo: $" + costo);
+            System.out.println("______________________________");
 
             total += costo;
         }
         System.out.println("Total recaudado: $" + total);
+        System.out.println("______________________________");
+
     }
 }
